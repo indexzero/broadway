3557bb4a6fbf892426af11250ef32f1758425339
indexzero <charlie.robbins@gmail.com>
2015-03-19 00:51:50 -0700

    [doc] Added simple examples to illustrate core `broadway@1.0.0` concepts.

---
+++ examples/base.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var express = require('express'),
+    App = require('../');
+
+//
+// Create an app with default http options,
+// mixin all express functions
+//
+var app = new App({ http: 8080 }, express());
+
+//
+// Do anything you want asynchronously before
+// the application starts.
+//
+app.preboot(function (app, options, next) {
+  console.log('Starting up...');
+  next();
+});
+
+//
+// Start listening on HTTP port passed in to
+// App when it was created above.
+//
+app.start(function (err) {
+  if (err) {
+    console.error('Error on startup: %s', err.message);
+    return process.exit(1);
+  }
+
+  console.log('Listening over HTTP on port %s', this.options.http);
+});
---
+++ examples/mixin.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var express = require('express'),
+    App = require('../');
+
+//
+// Create a new base App.
+//
+var app = new App();
+
+//
+// Then mixin express functionality later on. This
+// can be called multiple times, but will only define
+// a single property once on your app.
+//
+app.mixin(express());
+
+//
+// Do anything you want asynchronously before
+// the application starts.
+//
+app.preboot(function (app, options, next) {
+  console.log('Starting up...');
+  next();
+});
+
+//
+// Start listening on HTTP port passed in
+// explicitly at start-time below.
+//
+app.start({ http: 8080 }, function () {
+  if (err) {
+    console.error('Error on startup: %s', err.message);
+    return process.exit(1);
+  }
+
+  console.log('Listening over HTTP on port %s', this.options.http);
+});
